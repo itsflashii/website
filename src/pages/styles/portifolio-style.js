bcbdca7903594f267ecf371484d2772d269fb356
MarcoMacias <lanaalves291@gmail.com>
2021-11-03 22:13:48 -0300

    Inicio da responsividade portifolio-style (finished?)

--- src/pages/styles/portifolio-style.js
+++ src/pages/styles/portifolio-style.js
@@ -10,8 +10,6 @@ export const Container = styled.div`
   background-color: #060607;
   border-top: 1px solid rgba(115, 115, 128, 0.29);
   border-bottom: 1px solid rgba(115, 115, 128, 0.29);
-
-
  
 
   h1 {
@@ -42,6 +40,56 @@ export const Container = styled.div`
     
   }
 
+  @media only screen and (min-width: 1100px) and (max-width: 1255px) {
+    img {
+      width: 325px;
+      height: 290px;
+    }
+  }
+
+  @media only screen and (min-width: 0px) and (max-width: 1100px) {
+    img {
+      display: none;
+    }
+  }
+
+  @media only screen and (min-width: 625px) and (max-width: 745px) {
+    h1 {
+      left: 60px;
+    }
+
+    p {
+      left: 60px;
+    }
+
+  }
+
+  @media only screen and (min-width: 300px) and (max-width: 625px) {
+    h1 {
+      left: 30px;
+      width: 470px;
+      font-size: 24px;
+    }
+
+    p {
+      left: 30px;
+      width: 470px;
+      font-size: 18px;
+    }
+
+  }
+
+  @media only screen and (min-width: 300px) and (max-width: 505px) {
+    h1 {
+      width: 90%;
+    }
+
+    p {
+      width: 90%;
+    }
+
+  }
+
 `;
 
 export const Products = styled.div`
@@ -67,4 +115,33 @@ export const Products = styled.div`
   img:hover {
     border: 1px solid red;
   }
+
+  @media only screen and (min-width: 300px) and (max-width: 1095px) {
+    width: 75%;
+  }
+
+  @media only screen and (min-width: 600px) and (max-width: 800px) {
+    img {
+      width: 130px;
+    }
+
+    left: 40px;
+  }
+
+  @media only screen and (min-width: 400px) and (max-width: 600px) {
+    img {
+      width: 120px;
+    }
+
+    left: 40px;
+  }
+
+  @media only screen and (min-width: 280px) and (max-width: 400px) {
+    img {
+      width: 90px;
+    }
+  
+      left: 10px;
+  }
+
 `;
