6e205ea7f046787e8eb14202efa82d411ae838eb
Vítor Ribeiro <85264247+vitorsheik@users.noreply.github.com>
2021-11-19 16:23:41 -0300

    Update english.js

--- src/english.js
+++ src/english.js
@@ -68,9 +68,9 @@ function App() {
               largeText='Discover some of our main works.'
               smallText='Be part of this list too, contact Varsel.'
               link2='https://jelly-raisin-7a6.notion.site/SEV7N-CLIENT-bcab7d87bb7046e99902137583a67256'
-              link1='https://jelly-raisin-7a6.notion.site/Ops-a6afc3ae82b94b1b85eab194158bf071'
+              link1='https://jelly-raisin-7a6.notion.site/Go-Bank-58aef4059f944c1094a770951ed733ec'
               link3='https://jelly-raisin-7a6.notion.site/Sunrise-Roleplay-70d245f94fb24d28b15fcf004c6b6382'
-              link4='https://jelly-raisin-7a6.notion.site/Go-Bank-58aef4059f944c1094a770951ed733ec'
+              link4='https://jelly-raisin-7a6.notion.site/Ops-a6afc3ae82b94b1b85eab194158bf071'
               link5='https://jelly-raisin-7a6.notion.site/Ops-a6afc3ae82b94b1b85eab194158bf071'
               link6='https://jelly-raisin-7a6.notion.site/Ops-a6afc3ae82b94b1b85eab194158bf071'
             />
